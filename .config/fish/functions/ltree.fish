0e2057f57c9b3f5f46760947d1c331274ee70bf8
Umut Sahin Onder <umutsonder@gmail.com>
2024-09-24 23:16:05 +0200

    configured ltree alias

--- .config/fish/functions/ltree.fish
+++ .config/fish/functions/ltree.fish
@@ -1,4 +1,4 @@
-function ltree --wraps='lsd -tree' --wraps='lsd --tree' --description 'alias ltree=lsd --tree'
-  lsd --tree $argv
+function ltree --wraps='lsd -tree' --wraps='lsd --tree' --wraps='lsd --tree --depth 2' --description 'alias ltree=lsd --tree --depth 2'
+  lsd --tree --depth 2 $argv
         
 end
