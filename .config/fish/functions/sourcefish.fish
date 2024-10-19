bd27a2702958623d9b6022095016858052b7b3b1
Umut Sahin Onder <umutsonder@gmail.com>
2024-10-20 02:51:35 +0300

    added a function to source all fish files inside .config/fish/

---
+++ .config/fish/functions/sourcefish.fish
@@ -0,0 +1,4 @@
+function sourcefish --wraps='source ~/.config/fish/**/*.fish' --description 'alias sourcefish=source ~/.config/fish/**/*.fish'
+  source ~/.config/fish/**/*.fish $argv
+        
+end
