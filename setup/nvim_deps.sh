61c7973afd8743b0c06cae50a4cd0555b27e2637
Umut Sahin Onder <umutsonder@gmail.com>
2024-11-03 20:17:46 +0300

    removed peek.nvim dependencies because chromium takes too much time to install

--- setup/nvim_deps.sh
+++ setup/nvim_deps.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo apt install python3.11-venv -y # for mason packages
-sudo apt install chromium -y # for peek.nvim
+# sudo apt install chromium -y # for peek.nvim (takes too long to install)
 sudo apt install gcc -y
