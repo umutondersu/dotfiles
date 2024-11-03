407f6d1aaece9cc673140fde151ec037ba6188d5
Umut Sahin Onder <umutsonder@gmail.com>
2024-11-03 18:43:01 +0300

    removed installation from cat and ls functions since they are in install.sh now

--- .config/fish/functions/cat.fish
+++ .config/fish/functions/cat.fish
@@ -1,8 +1,3 @@
 function cat
-    if type -q bat
         bat $argv
-    else
-        echo "installing bat"
-        wget -O /tmp/bat-musl_0.24.0_amd64.deb https://github.com/sharkdp/bat/releases/download/v0.24.0/bat-musl_0.24.0_amd64.deb && sudo dpkg -i /tmp/bat-musl_0.24.0_amd64.deb && rm /tmp/bat-musl_0.24.0_amd64.deb && /usr/bin/bat $argv
-    end
 end
--- .config/fish/functions/ls.fish
+++ .config/fish/functions/ls.fish
@@ -1,12 +1,3 @@
 function ls
-    if type -q lsd
         lsd $argv
-    else
-        if ! type -q cargo
-            echo "Cargo is not installed! install and try again"
-            return
-        end
-        echo "installing lsd"
-        cargo install lsd
-    end
 end
