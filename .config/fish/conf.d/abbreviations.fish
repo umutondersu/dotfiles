cf7f6f910a962b50487b498a4bcef0cc5b3b5538
Umut Sahin Onder <umutsonder@gmail.com>
2024-11-13 16:32:40 +0100

    new abbr for creating mssql container

--- .config/fish/conf.d/abbreviations.fish
+++ .config/fish/conf.d/abbreviations.fish
@@ -16,3 +16,8 @@ abbr --add python python3
 abbr --add v nvim
 abbr --add V --position anywhere "&& nvim"
 abbr --add ds devpod ssh
+abbr --add dockersql docker run -e "'ACCEPT_EULA=Y'" -e "'MSSQL_SA_PASSWORD=password'" \
+   -p 1433:1433 --name sql1 --hostname sql1 \
+   -v sqlvolume:/var/opt/mssql \
+   -d \
+   mcr.microsoft.com/mssql/server:2022-latest
