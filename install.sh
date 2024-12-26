9818b133bf3dfe2dd6d04d1cd37e502d0342eb42
Umut Sahin Onder <umutsonder@gmail.com>
2024-12-26 01:38:09 +0100

    updated install scripts

--- install.sh
+++ install.sh
@@ -8,9 +8,4 @@
 git clone https://github.com/umutondersu/nvim.git ~/.config/nvim
 sudo chsh -s /usr/bin/fish $USER
 
-if [ "$DEVCONTAINER" = "true" ] || [ "$CODESPACES" = "true" ] || [ "$REMOTE_CONTAINERS" = "true" ]; then
-		mkdir -p ~/.gnupg
-		echo "use-standard-socket" > ~/.gnupg/gpg-agent.conf
-else
-	./setup/tmux_setup.sh
-fi
+./setup/tmux.sh
--- setup/tmux.sh
+++ setup/tmux.sh
@@ -2,24 +2,14 @@
 
 TPM_DIR=~/.tmux/plugins/tpm
 
-# Check if TPM directory exists
+# Check if TPM directory exists if not clone it
 if [ ! -d "$TPM_DIR" ]; then
     echo "Cloning tmux-plugins/tpm into $TPM_DIR..."
     git clone https://github.com/tmux-plugins/tpm "$TPM_DIR"
     echo "Clone completed successfully."
-
-    echo "To install plugins, follow these steps:"
-    echo "1. Start tmux by typing 'tmux' in your terminal."
-    echo "2. Press <Ctrl-Space> + I to fetch and install plugins using TPM."
-    echo "3. Restart tmux to source the updated configuration."
-
-else
-    echo "TPM directory already exists. No need to clone."
-
-     if [ -d "~/.config/tmux/plugins" ]; then
-        echo "To install plugins, follow these steps:"
-        echo "1. Start tmux by typing 'tmux' in your terminal."
-        echo "2. Press <Ctrl-Space> + I to fetch and install plugins using TPM."
-        echo "3. Restart tmux to source the updated configuration."
-    fi
 fi
+
+echo "To install plugins, follow these steps:"
+echo "1. Start tmux by typing 'tmux' in your terminal."
+echo "2. Press <Ctrl-Space> + I to fetch and install plugins using TPM."
+echo "3. Restart tmux to source the updated configuration."
