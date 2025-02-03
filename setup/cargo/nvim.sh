6562b5b0db55fd28fdf0c1f2be487039375be04c
Umut Sahin Onder <umutsonder@gmail.com>
2025-02-03 23:17:49 +0100

    updated how fdfind is installed and used for snacks.nvim

--- setup/cargo/nvim.sh
+++ setup/cargo/nvim.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 ./setup/nvim_deps.sh
+cargo install fd-find
 cargo install --git https://github.com/MordechaiHadad/bob.git
-bob use 0.10.1
+bob install latest
+bob use latest
 export PATH=$HOME/.local/share/bob/nvim-bin:$PATH
--- setup/wo_cargo/nvim.sh
+++ setup/wo_cargo/nvim.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 ./setup/nvim_deps.sh
+npm install -g fd-find
 #NOTE: neovim has to be installed before this by either docker or devcontainer feature
