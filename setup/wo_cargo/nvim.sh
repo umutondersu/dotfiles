02c3b1886c73656c1b35fbe96c4b64ad067ccef1
Umut Sahin Onder <umutsonder@gmail.com>
2025-02-16 21:42:40 +0100

    Removed nvim installation because its too slow

--- setup/wo_cargo/nvim.sh
+++ setup/wo_cargo/nvim.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
 ./setup/nvim_deps.sh
-git clone -b v0.10.4 https://www.github.com/neovim/neovim.git $HOME/personal/neovim
-sudo apt install -y cmake gettext lua5.1 liblua5.1-0-dev
-cd $HOME/personal/neovim
-make CMAKE_BUILD_TYPE=RelWithDebInfo
-sudo make install
-cd $HOME/dotfiles
