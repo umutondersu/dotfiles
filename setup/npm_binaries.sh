17f2f7319362d4a9fd7f54b3c67c3032ff8f1920
Umut Sahin Onder <umutsonder@gmail.com>
2025-02-16 23:55:14 +0100

    fixing why npm installs do not work

--- setup/npm_binaries.sh
+++ setup/npm_binaries.sh
@@ -1,4 +1,17 @@
-#!/bin/fish
+#!/bin/bash
 
-exec fish
+# Ensure nvm is loaded
+export NVM_DIR="/usr/local/share/nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+
+# Get the node version from fish
+node_version=$(fish -c 'echo $nvm_default_version')
+
+# Use the specific Node version
+nvm use $node_version || nvm install $node_version
+
+# Ensure npm is available
+export PATH="$NVM_DIR/versions/node/$(nvm current)/bin:$PATH"
+
+# Install npm packages
 npm install -g fd-find
