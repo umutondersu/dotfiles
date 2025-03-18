1d65151de49cb91bebe0b3005da131d062b63396
Umut Sahin Onder <umutsonder@gmail.com>
2025-03-18 02:16:34 +0100

    refactor(fish): refactored some abbrs to be functions and vice versa

--- .config/fish/conf.d/abbreviations.fish
+++ .config/fish/conf.d/abbreviations.fish
@@ -10,45 +10,48 @@ if type -q devpod
    abbr --add dps devpod ssh
    abbr --add dpu devpod up .
    abbr --add dpd devpod delete
+   abbr --add dpS devpod stop
 end
 
 abbr --add soundrestart systemctl --user restart wireplumber pipewire pipewire-pulse
 abbr --add onedrivelog journalctl --user-unit=onedrive -f
-abbr --add ga git add
-abbr --add gco git checkout
 abbr --add claer clear
 abbr --add lst lsd --tree --depth 2
-abbr --add please sudo
 abbr --add python python3
 abbr --add v nvim
 abbr --add V --position anywhere "&& nvim"
-abbr --add kp sudo kill -9
+abbr --add fkill sudo kill -9
+abbr --add sourcefish source ~/.config/fish/**/*.fish
 
-abbr --add dopen_webui docker run -d \
-            --name open-webui \
-            -p 3001:8080 \
-            -e WEBUI_AUTH=False \
-            -v open-webui:/app/backend/data \
-            --restart unless-stopped \
-            --add-host host.docker.internal:host-gateway \
-            ghcr.io/open-webui/open-webui:mai
+# Abbreviations for starting up servers with docker
+if type -q docker
+   #open-webui
+   abbr --add open_webuid docker run -d \
+               --name open-webui \
+               -p 3001:8080 \
+               -e WEBUI_AUTH=False \
+               -v open-webui:/app/backend/data \
+               --restart unless-stopped \
+               --add-host host.docker.internal:host-gateway \
+               ghcr.io/open-webui/open-webui:mai
 
-# Abbreviations for starting up sql servers
-abbr --add dmssql docker run -p 1433:1433 --name mssql --hostname mssql \
-   -d \
-   mcr.microsoft.com/mssql/server:2022-latest \
-   -v sqlvolume:/var/opt/mssql \
-   -e "'ACCEPT_EULA=Y'" -e "'MSSQL_SA_PASSWORD=password'"
+   #SQL
+   abbr --add mssqld docker run -p 1433:1433 --name mssql --hostname mssql \
+      -d \
+      mcr.microsoft.com/mssql/server:2022-latest \
+      -v sqlvolume:/var/opt/mssql \
+      -e "'ACCEPT_EULA=Y'" -e "'MSSQL_SA_PASSWORD=password'"
 
-abbr --add dpsql docker run -d \
-   -p 5432:5432 \
-   --name postgres \
-   -v pgdata:/var/lib/postgresql/data \
-   -e POSTGRES_PASSWORD=password  \
-   postgres \
+   abbr --add psqld docker run -d \
+      -p 5432:5432 \
+      --name postgres \
+      -v pgdata:/var/lib/postgresql/data \
+      -e POSTGRES_PASSWORD=password  \
+      postgres \
 
-abbr --add dmysql docker run -d \
-   --name mysql-container \
-   -e MYSQL_ROOT_PASSWORD=your_password \
-   -p 3306:3306 \
-   mysql:latest
+   abbr --add mysqld docker run -d \
+      --name mysql-container \
+      -e MYSQL_ROOT_PASSWORD=your_password \
+      -p 3306:3306 \
+      mysql:latest
+end
---
+++ .config/fish/functions/please.fish
@@ -0,0 +1,4 @@
+function please --description "Politely execute a command with sudo"
+    sudo $argv
+end
+
