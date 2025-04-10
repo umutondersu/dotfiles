291fab6465850eed74bc536210418e08b06efde1
Umut Sahin Onder <umutsonder@gmail.com>
2025-04-11 01:06:05 +0200

    Better xfreerdp command

--- .config/fish/functions/win11.fish
+++ .config/fish/functions/win11.fish
@@ -9,5 +9,5 @@ function win11
     end
 
     # Execute xfreerdp command
-    xfreerdp -grab-keyboard /v:192.168.122.29 /u:qorcialwolf /p:susam123 /size:100% /d: /dynamic-resolution +gfx-progressive
+    xfreerdp -grab-keyboard /v:192.168.122.29 /u:qorcialwolf /p:susam123 /scale:180 /d: /dynamic-resolution +gfx-progressive /sound:sys:alsa /w:1920 /h:1080
 end
