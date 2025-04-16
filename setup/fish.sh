eb66b8f1cf46c25612974e65c864a193f0097abb
Umut Sahin Onder <umutsonder@gmail.com>
2025-04-16 14:47:07 +0200

    removed software-properties-common dependency from fish installation

--- setup/fish.sh
+++ setup/fish.sh
@@ -14,7 +14,6 @@ case $DISTRO in
     ubuntu|pop)
         # Ubuntu/Pop!_OS installation using PPA
         sudo apt update
-        sudo apt install -y software-properties-common
         sudo apt-add-repository -y ppa:fish-shell/release-4
         sudo apt update
         sudo apt install -y fish
