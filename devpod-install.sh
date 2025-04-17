f3f02d9818e6a18ad92622cce6f6c0826a81bc77
Umut Sahin Onder <umutsonder@gmail.com>
2025-04-17 16:03:22 +0200

    Double quote in install scripts to prevent globbing and word splitting.

--- devpod-install.sh
+++ devpod-install.sh
@@ -7,7 +7,7 @@
 ./setup/npm_binaries.sh
 
 git clone https://github.com/umutondersu/nvim.git ~/.config/nvim
-sudo chsh -s /usr/bin/fish $USER
+sudo chsh -s /usr/bin/fish "$USER"
 
 mkdir -p ~/.gnupg
 echo "use-standard-socket" > ~/.gnupg/gpg-agent.conf
--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@
 ./setup/npm_binaries.sh
 
 git clone https://github.com/umutondersu/nvim.git ~/.config/nvim
-sudo chsh -s /usr/bin/fish $USER
+sudo chsh -s /usr/bin/fish "$USER"
 
 ./setup/tmux.sh
 ./setup/nerd-dictation.sh
