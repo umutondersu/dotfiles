a075f2aa5341ae96023f8242a26c5942aa954375
Umut Sahin Onder <umutsonder@gmail.com>
2025-06-03 18:04:36 +0200

    replace posting.sh with uv.sh

--- setup/binaries/uv.sh
+++ setup/binaries/uv.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-# uv needs to be installed first
-# quick install on MacOS/Linux
 curl -LsSf https://astral.sh/uv/install.sh | sh
+
+uv python install --default --preview 3.10
 
 # install Posting (will also quickly install Python 3.12 if needed)
 uv tool install --python 3.12 posting
+
