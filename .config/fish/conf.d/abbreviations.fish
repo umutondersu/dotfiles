1c74b9e0cf95dc095a99e32d5c5cd31bf7e964e2
Umut Sahin Onder <umutsonder@gmail.com>
2025-06-07 18:08:11 +0200

    format abbreviations.fish

--- .config/fish/conf.d/abbreviations.fish
+++ .config/fish/conf.d/abbreviations.fish
@@ -6,16 +6,16 @@ if type -q rip
 end
 
 if type -q devpod
-   abbr --add dp devpod
-   abbr --add dps devpod ssh
-   abbr --add dpu devpod up .
-   abbr --add dpd devpod delete
-   abbr --add dpS devpod stop
+    abbr --add dp devpod
+    abbr --add dps devpod ssh
+    abbr --add dpu devpod up .
+    abbr --add dpd devpod delete
+    abbr --add dpS devpod stop
 end
 
 if type -q lsd
-   abbr --add lst ls --tree --depth 2
-   abbr --add lt 'ls -I node_modules -I dist -I build -I .idea -I .vscode -I target -I out -I .pytest_cache -I .cache -I __pycache__ --tree --depth 2'
+    abbr --add lst ls --tree --depth 2
+    abbr --add lt 'ls -I node_modules -I dist -I build -I .idea -I .vscode -I target -I out -I .pytest_cache -I .cache -I __pycache__ --tree --depth 2'
 end
 abbr --add l ls
 abbr --add ll ls -lg
@@ -33,33 +33,32 @@ abbr --add update 'sudo apt update && sudo apt upgrade -y && flatpak update -y'
 
 # Abbreviations for starting up servers with docker
 if type -q docker
-   #open-webui
-   abbr --add open_webuid docker run -d \
-               --name open-webui \
-               -p 3001:8080 \
-               -e WEBUI_AUTH=False \
-               -v open-webui:/app/backend/data \
-               --restart unless-stopped \
-               --add-host host.docker.internal:host-gateway \
-               ghcr.io/open-webui/open-webui:mai
+    #open-webui
+    abbr --add open_webuid docker run -d \
+        --name open-webui \
+        -p 3001:8080 \
+        -e WEBUI_AUTH=False \
+        -v open-webui:/app/backend/data \
+        --restart unless-stopped \
+        --add-host host.docker.internal:host-gateway \
+        ghcr.io/open-webui/open-webui:mai
 
-   #SQL
-   abbr --add mssqld docker run -p 1433:1433 --name mssql --hostname mssql \
-      -d \
-      mcr.microsoft.com/mssql/server:2022-latest \
-      -v sqlvolume:/var/opt/mssql \
-      -e "'ACCEPT_EULA=Y'" -e "'MSSQL_SA_PASSWORD=password'"
+    #SQL
+    abbr --add mssqld docker run -p 1433:1433 --name mssql --hostname mssql \
+        -d \
+        mcr.microsoft.com/mssql/server:2022-latest \
+        -v sqlvolume:/var/opt/mssql \
+        -e "'ACCEPT_EULA=Y'" -e "'MSSQL_SA_PASSWORD=password'"
 
-   abbr --add psqld docker run -d \
-      -p 5432:5432 \
-      --name postgres \
-      -v pgdata:/var/lib/postgresql/data \
-      -e POSTGRES_PASSWORD=password  \
-      postgres \
-
-   abbr --add mysqld docker run -d \
-      --name mysql-container \
-      -e MYSQL_ROOT_PASSWORD=your_password \
-      -p 3306:3306 \
-      mysql:latest
+    abbr --add psqld docker run -d \
+        -p 5432:5432 \
+        --name postgres \
+        -v pgdata:/var/lib/postgresql/data \
+        -e POSTGRES_PASSWORD=password \
+        postgres
+    abbr --add mysqld docker run -d \
+        --name mysql-container \
+        -e MYSQL_ROOT_PASSWORD=your_password \
+        -p 3306:3306 \
+        mysql:latest
 end
