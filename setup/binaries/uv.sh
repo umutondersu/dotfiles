958206ed04046c2834e7013b0d4ecc5b6749f9bf
Umut Sahin Onder <umutsonder@gmail.com>
2025-06-11 12:07:23 +0200

    fix uv command not found

--- setup/binaries/uv.sh
+++ setup/binaries/uv.sh
@@ -2,6 +2,8 @@
 
 curl -LsSf https://astral.sh/uv/install.sh | sh
 
+export PATH="$HOME/.local/bin:$PATH"
+
 uv python install --default --preview 3.10
 
 # install Posting (will also quickly install Python 3.12 if needed)
