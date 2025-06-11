fdc03409cbd1c3eb60f79359f4e0b3752b9c5ecc
Umut Sahin Onder <umutsonder@gmail.com>
2025-06-11 12:03:31 +0200

    devpod fixes

--- devpod-install.sh
+++ devpod-install.sh
@@ -9,5 +9,8 @@
 git clone https://github.com/umutondersu/nvim.git ~/.config/nvim
 sudo chsh -s /usr/bin/fish "$USER"
 
+# Container Specific Setup
+sudo apt install xclip
+fish -c 'set -Ux TERM xterm-256color'
 mkdir -p ~/.gnupg
 echo "use-standard-socket" > ~/.gnupg/gpg-agent.conf
