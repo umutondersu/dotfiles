a7f85dc4dc56745eaa57913aebed4b3d608326d7
Umut Sahin Onder <umutsonder@gmail.com>
2025-08-30 21:47:40 +0200

    Move fuck to uv

--- setup/binaries/fuck.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-if command -v python3 >/dev/null 2>&1 && (command -v pip3 >/dev/null 2>&1 || (sudo apt-get update && sudo apt-get install -y python3-pip)); then
-    pip3 install thefuck --user
-fi
