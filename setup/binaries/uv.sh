66c9c8cae56aced5bdd86035be8812e6c21dcf45
Umut Sahin Onder <umutsonder@gmail.com>
2025-09-04 03:22:14 +0200

    Update npm version to 22.19

--- setup/binaries/uv.sh
+++ setup/binaries/uv.sh
@@ -7,7 +7,6 @@ export PATH="$HOME/.local/bin:$PATH"
 uv python install --default --preview 3.10
 
 # Install Tools
-# install Posting (will also quickly install Python 3.12 if needed)
 uv tool install --python 3.12 posting
 uv tool install thefuck --python=python3.11
 uv tool install tldr
