d52bd0e0851be5a81571a0fb96156640433ea0cf
Umut Sahin Onder <umutsonder@gmail.com>
2025-10-19 23:28:59 +0200

    Delete avante alias

--- .config/fish/functions/avante.fish
+++
@@ -1,4 +0,0 @@
-function avante --wraps='nvim . -c "lua vim.defer_fn(function()require(\\"avante.api\\").zen_mode()end, 100)"' --description 'alias avante=nvim . -c "lua vim.defer_fn(function()require(\\"avante.api\\").zen_mode()end, 100)"'
-  nvim . -c "lua vim.defer_fn(function()require(\"avante.api\").zen_mode()end, 100)" $argv
-        
-end
