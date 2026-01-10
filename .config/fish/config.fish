d89d5682c3cf786c5a52f26743dbfc9260a3b82c
Umut Sahin Onder <umutsonder@gmail.com>
2026-01-10 18:50:19 +0100

    update fish config for 4.3

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,8 @@
 set fish_greeting
 
+# Set vi key bindings as default (migrated from Fish 4.3 frozen file)
+set --global fish_key_bindings fish_vi_key_bindings
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set -x GPG_TTY (tty)
