f0ad2e6ce3e5642ed5d50fdff7710ec048190eba
Umut Sahin Onder <umutsonder@gmail.com>
2026-02-17 13:01:14 +0100

    add go binaries to fish path

--- .config/fish/conf.d/path.fish
+++ .config/fish/conf.d/path.fish
@@ -2,4 +2,5 @@ fish_add_path /usr/bin
 fish_add_path ~/.local/bin
 fish_add_path ~/.local/kitty.app/bin
 fish_add_path ~/.local/share/nvim/mason/bin
-fish_add_path /home/qorcialwolf/.opencode/bin
+fish_add_path ~/.opencode/bin
+fish_add_path ~/go/bin
