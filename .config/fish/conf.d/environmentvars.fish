b05b129b8f715e57ae34248768459b0d6b5555ec
Umut Sahin Onder <umutsonder@gmail.com>
2026-02-20 14:50:16 +0100

    set manpager to neovim only if its on the path

--- .config/fish/conf.d/environmentvars.fish
+++ .config/fish/conf.d/environmentvars.fish
@@ -1,6 +1,8 @@
-set -gx EDITOR nvim
 set -gx GOTOOLCHAIN auto
 set -gx TERM xterm-256color
 set -gx TMPDIR /tmp
 set -gx XDG_RUNTIME_DIR /tmp/
-set -gx MANPAGER "sh -c 'col -b | nvim -R -c \"set ft=man nomod nolist\" -'"
+if type -q nvim
+    set -gx EDITOR nvim
+    set -gx MANPAGER "sh -c 'col -b | nvim -R -c \"set ft=man nomod nolist\" -'"
+end
