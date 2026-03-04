806ab0e6da7e6a05005a4c323cc98adabf133780
Umut Sahin Onder <umutsonder@gmail.com>
2026-03-04 19:06:08 +0100

    Remove XDG_RUNTIME_DIR variable

--- .config/fish/conf.d/environmentvars.fish
+++ .config/fish/conf.d/environmentvars.fish
@@ -1,7 +1,6 @@
 set -gx GOTOOLCHAIN auto
 set -gx TERM tmux-256color
 set -gx TMPDIR /tmp
-set -gx XDG_RUNTIME_DIR /tmp/
 if type -q nvim
     set -gx EDITOR nvim
     set -gx MANPAGER "sh -c 'col -b | nvim -R -c \"set ft=man nomod nolist\" -'"
