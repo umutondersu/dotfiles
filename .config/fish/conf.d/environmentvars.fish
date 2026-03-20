22c9fa0ef8607b17e94986691709116be2f88d51
Umut Sahin Onder <umutsonder@gmail.com>
2026-03-20 17:50:50 +0100

    add DEVBOX_PATH env_variable

--- .config/fish/conf.d/environmentvars.fish
+++ .config/fish/conf.d/environmentvars.fish
@@ -1,6 +1,7 @@
 set -gx GOTOOLCHAIN auto
 set -gx TERM tmux-256color
 set -gx TMPDIR /tmp
+set -gx DEVBOX_PATH "$DEVBOX_PACKAGES_DIR/bin/"
 if type -q nvim
     set -gx EDITOR nvim
     set -gx MANPAGER "sh -c 'col -b | nvim -R -c \"set ft=man nomod nolist\" -'"
