cf570cf8971b7bd6a3bbd578b7c914f5b2d6e5c7
Umut Sahin Onder <umutsonder@gmail.com>
2026-03-21 23:06:49 +0100

    Add a script to switch audio sink for pulsewire

---
+++ .scripts/switch-audio-sink.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+mapfile -t sinks < <(pactl list short sinks | awk '{print $2}' | grep -v easyeffects)
+
+sink_current=$(pactl get-default-sink)
+
+for i in "${!sinks[@]}"; do
+  if [[ "${sinks[$i]}" == "$sink_current" ]]; then
+    next=$(( (i + 1) % ${#sinks[@]} ))
+    pactl set-default-sink "${sinks[$next]}"
+    exit 0
+  fi
+done
+
+# Current sink not in list; fall back to first
+pactl set-default-sink "${sinks[0]}"
