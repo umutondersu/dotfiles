f57c2a002d1e492e75963ed4f0273dbd4da0e2f6
Umut Önder <93402298+umutondersu@users.noreply.github.com>
2026-05-15 17:02:03 +0200

    Clear old fish user paths

--- .config/fish/conf.d/path.fish
+++ .config/fish/conf.d/path.fish
@@ -1,6 +1,4 @@
 fish_add_path /usr/bin
 fish_add_path ~/.local/bin
-fish_add_path ~/.local/kitty.app/bin
-fish_add_path ~/.local/share/nvim/mason/bin
-fish_add_path ~/.opencode/bin
+fish_add_path ~/bin
 fish_add_path ~/go/bin
