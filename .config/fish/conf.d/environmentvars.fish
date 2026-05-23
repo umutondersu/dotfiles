1ef7314b66e72bcc6c204a0bacd17c416c18fe3d
Umut Sahin Onder <umutsonder@gmail.com>
2026-05-23 22:06:35 +0200

    Improve color settings + add SUDO EDITOR env var for nvim

--- .config/fish/conf.d/environmentvars.fish
+++ .config/fish/conf.d/environmentvars.fish
@@ -1,9 +1,8 @@
 set -gx GOTOOLCHAIN auto
-set -gx TERM tmux-256color
 set -gx TMPDIR /tmp
-set -gx DEVBOX_PATH "$DEVBOX_PACKAGES_DIR/bin/"
 set -gx fisher_path $HOME/.local/share/fisher
 if type -q nvim
     set -gx EDITOR nvim
     set -gx MANPAGER "nvim +Man! -"
+    set -gx SUDO_EDITOR (which nvim)
 end
