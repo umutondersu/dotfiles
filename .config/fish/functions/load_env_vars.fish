e0f7168183e054b7fe83b01eb524f423671134a3
Umut Sahin Onder <umut_sahin.oender@digits.schwarz>
2026-08-05 17:12:23 +0200

    Resolve fish warnings

--- .config/fish/functions/load_env_vars.fish
+++ .config/fish/functions/load_env_vars.fish
@@ -1,8 +1,8 @@
 function load_env_vars -d "Load variables in a .env file"
-    if not test -f $argv
+    if test (count $argv) -eq 0; or not test -f $argv[1]
         return
     end
-    set lines (cat $argv | string split -n '\n' | string match -vre '^#')
+    set lines (cat $argv[1] | string split -n '\n' | string match -vre '^#')
     for line in $lines
         set arr (string split -n -m 1 = $line)
         if test (count $arr) -ne 2
--- .config/fish/functions/y.fish
+++ .config/fish/functions/y.fish
@@ -1,7 +1,7 @@
 function y
     set tmp (mktemp -t "yazi-cwd.XXXXXX")
     command yazi $argv --cwd-file="$tmp"
-    if read -z cwd <"$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
+    if read -q -z cwd <"$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
         builtin cd -- "$cwd"
     end
     rm -f -- "$tmp"
